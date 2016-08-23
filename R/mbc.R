4df58257d60db6bd964c057db7e6a828746e17a9
Joshua Rosenberg <jrosen@msu.edu>
2016-08-23 10:40:52 -0400

    add functional mbc

--- R/mbc.R
+++ R/mbc.R
@@ -3,9 +3,9 @@
 # library(mclust)
 # 
 create_profiles_lpa <- function(prepared_data, G = 9, cluster_names = NULL, print_status = T){
-    
+
     if(is.null(cluster_names)){
-        cluster_names <- paste0("Cluster ", 1:G)
+        cluster_names <- paste0("Cluster ", G)
     }
     variable_names <- attributes(prepared_data)$variable_names
     # Create profiles
@@ -14,9 +14,11 @@ create_profiles_lpa <- function(prepared_data, G = 9, cluster_names = NULL, prin
     out <- list()
     prepared_data_ss <- data.frame(matrix(unlist(prepared_data), ncol = length(prepared_data), byrow = F))
 
-    tmp_BIC = mclust::mclustBIC(prepared_data_ss)
+    tmp_BIC = mclust::mclustBIC(prepared_data_ss, G = G)
+
     out[[1]] <- tmp_BIC # fit indices
-    model1 <- mclust::Mclust(data = prepared_data_ss, x = tmp_BIC, G)
+    
+    model1 <- mclust::Mclust(data = prepared_data_ss, x = tmp_BIC, G = G)
 
     out[[2]] <- model1 # model output
 
@@ -44,9 +46,49 @@ create_profiles_lpa <- function(prepared_data, G = 9, cluster_names = NULL, prin
         print("### 4. Cluster centroid table ###")
     }
     
+    attributes(out) <- list(df = prepared_data_ss)
     return(out)
 
 }
+
+
+explore_factors_lpa_test <- function(factor_data_frame, 
+                                     factor_to_explore, 
+                                     variable_to_find_proportion = NULL, 
+                                     cluster_names = NULL,
+                                     print_status = T,
+                                     cluster_assignments,
+                                     cases_to_keep,
+                                     variable_names,
+                                     cleaned_data){
+    out <- list()
+    factor_data_frame[] <- lapply(factor_data_frame, factor)
+    data <- merge_assignments_and_factors(cluster_assignments, cases_to_keep, factor_data_frame)
+    data_for_descriptive_stats <- data.frame(cleaned_data, data)
+    dummy_coded_data <- dummmy_code_cluster_assignments(data)
+    out[[1]] <- create_crosstab(data, factor_to_explore)
+    out[[2]] <- create_raw_data(dummy_coded_data, factor_to_explore, variable_to_find_proportion)
+    out[[3]] <- create_processed_data(out[[2]], factor_to_explore, variable_to_find_proportion)
+    out[[4]] <- create_plot_to_explore_factors(out[[3]], factor_to_explore, cluster_names)
+    out[[5]] <- find_n(out[[2]], factor_to_explore)
+    out[[6]] <- create_compare_anova(out[[2]], variable_to_find_proportion, cluster_names, factor_to_explore)
+    # out[[7]] <- create_compare_manova()
+    
+    if(print_status == T){
+        print("### Created the following output ... ")
+        print("### 1. Comparison table ###")
+        print("### 2. Processed data: Raw ###")
+        print("### 3. Processed data: Summary ###")
+        print("### 4. ggplot2 object  ###")
+        print("### 5. Number by factor  ###")
+        print("### 6. ANOVA [[1]] and Tukey HSD [[2]] ###")
+        # print("### 7. MANOVA ###")
+    }
+    attributes(out) <- list(cleaned_df = data_for_descriptive_stats, data_attr = data)
+    invisible(out)
+    
+}
+
 
 # 
 # x <- create_profiles_lpa(prepared_data, G = 7)
