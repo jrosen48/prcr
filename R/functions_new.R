e161ddb034d2a85783ea4229e0c5e1b7215c42d0
Joshua Rosenberg <jrosen@msu.edu>
2017-08-24 17:04:02 -0400

    first working version of test branch

--- R/functions_new.R
+++ R/functions_new.R
@@ -2,62 +2,21 @@ library(tidyverse)
 
 # functions_new.R
 
-scale_vector <- function(x) {
-    x / stats::sd(x, na.rm = TRUE)
-}
-
-center_vector <- function(x) {
-    x - mean(x, na.rm = TRUE)
-}
-
-center_and_scale_vector <- function(x) {
-    if (stats::sd(x, na.rm = TRUE) == 0) {
-        x - mean(x, na.rm = TRUE)
-    } else {
-        (x - mean(x, na.rm = TRUE)) / stats::sd(x, na.rm = TRUE)
-    }
-}
-
-distance_function <- function(x, distance_metric){
-    if (distance_metric == "squared_euclidean") {
-        distance <- stats::dist(x, method = "euclidean")
-        distance <- distance ^ 2
-    } else {
-        distance <- stats::dist(x, method = distance_metric)
-    }
-    return(distance)
-}
-
-distance_function <- function(x, distance_metric){
-    if (distance_metric != "squared_euclidean") {
-        distance <- stats::dist(x, method = distance_metric)
-    } 
-    else {
-        distance <- stats::dist(x, method = "euclidean")
-        distance <- distance ^ 2
-    }
-    
-    distance
-}
-
-hclust_to_kmeans_function <- function(df, hclust_output, n_profiles){
-    h_clust_classification <- stats::cutree(hclust_output, n_profiles)
-    df %>% 
-        mutate(classification = h_clust_classification) %>% 
-        group_by(classification) %>% 
-        summarize_all(mean)
-}
-
-possibly_kmeans <- possibly(kmeans, NA)
-
 # prcr <- function() {
 #     structure(data.frame(), class = "prcr")
 # }
 
 prepare_data <- function(df, ..., to_center, to_scale){
     if (!is.data.frame(df)) stop("df must be a data.frame (or tibble)")
+    
     df <- tibble::as_tibble(df)
-    df_ss <- dplyr::select(df, ...)
+    
+    if (rlang::dots_n(...) > 1) {
+        df_ss <- dplyr::select(df, ...)
+    } else {
+        df_ss <- dplyr::select(df, everything())
+    }
+    
     cases_to_keep <- stats::complete.cases(df_ss) # to use later for comparing function to index which cases to keep
     df_complete <- df_ss[cases_to_keep, ] # removes incomplete cases
     
@@ -92,25 +51,31 @@ prepare_data <- function(df, ..., to_center, to_scale){
     return(df_complete)
 }
 
-is.kmeans <- function(x) inherits(x, "kmeans")
 
-cluster_observations <- function(df,
-                                 ...,
-                                 n_profiles,
-                                 center_data = TRUE,
-                                 scale_data = TRUE,
-                                 distance_metric = "euclidean",
-                                 linkage = "complete",
-                                 to_return = "clustered_data"){
+create_profiles <- function(df,
+                            ...,
+                            n_profiles,
+                            center_data = TRUE,
+                            scale_data = TRUE,
+                            distance_metric = "euclidean",
+                            linkage = "complete",
+                            to_return = "clustered_data"){
+    
     df <- prepare_data(df, ..., to_center = center_data, to_scale = scale_data)
     
     distance_matrix <- distance_function(df, distance_metric)
-    clustered_data <- stats::hclust(distance_matrix, method = linkage) # hierarhical clustering
+    
+    hclust_output <- stats::hclust(distance_matrix, method = linkage) # hierarhical clustering
+    
+    attributes(df)$hclust_output <- hclust_output
+    
     message(paste0("Hierarchical clustering carried out on: ", nrow(df), " cases"))
-    #names(clustered_data)[[3]] <- "hierarchical_clustering_output"
-    starting_centroids <- hclust_to_kmeans_function(df, clustered_data, n_profiles = n_profiles)
+    #names(hclust_output)[[3]] <- "hierarchical_clustering_output"
+    starting_centroids <- hclust_to_kmeans_function(df, hclust_output, n_profiles = n_profiles)
     
     kmeans_output <- possibly_kmeans(df, dplyr::select(starting_centroids, -classification)) # Fits k-means algorithm with hierarchical vals as start value
+    
+    attributes(df)$kmeans_output <- kmeans_output
     
     if (length(kmeans_output) == 1) {
         return(kmeans_output)
@@ -123,7 +88,7 @@ cluster_observations <- function(df,
     }
     #names(kmeans_output) <- "kmeans_clustering_output"
     if (is.kmeans(kmeans_output)) {
-        attributes(clustered_data)$n_profiles <- n_profiles
+        attributes(hclust_output)$n_profiles <- n_profiles
         message("Clustered data: Using a ", n_profiles, " cluster solution")    
     }
     
@@ -160,15 +125,332 @@ cluster_observations <- function(df,
     
 }
 
-x <- cluster_observations(iris, 
-                          Sepal.Length, Sepal.Width, Petal.Length, Petal.Width,
-                          n_profiles = 3,
-                          to_return="raw_selected_data")
+# ADD SD
+
+#' Return plot of cluster centroids
+#' @details Returns ggplot2 plot of cluster centroids
+#' @param x A `prcr` object
+#' @param ... Additional arguments
+#' @return A ggplot2 object
+#' @export
+
+plot_profiles<- function(x, ...){
+    # if (is.prcr(x) != TRUE) stop("is not a `prcr` object")
+    
+    x %>% 
+        group_by(profile) %>% 
+        summarize_all(mean) %>% 
+        gather(var, val, -profile) %>%
+        ggplot(aes(x = profile, y = val, fill = var)) +
+        geom_col(position = "dodge")
+}
 
 # Test code
 
-iris %>% 
-    cluster_observations(Sepal.Length, Sepal.Width, Petal.Length, Petal.Width,
-                         n_profiles = 3)
+# iris %>% 
+#     select(-Species) %>% 
+#     create_profiles(n_profiles = 3)
 
+#' ###Plot R^2 (r-squared) values for a range of number of profiles
+#' @details Returns ggplot2 plot of cluster centroids
+#' @param df with two or more columns with continuous variables
+#' @param ... unquoted variable names separated by commas
+#' @param to_center (TRUE or FALSE) for whether to center the raw data with M = 0
+#' @param to_scale Boolean (TRUE or FALSE) for whether to scale the raw data with SD = 1
+#' @param distance_metric Distance metric to use for hierarchical clustering; "squared_euclidean" is default but more options are available (see ?hclust)
+#' @param linkage Linkage method to use for hierarchical clustering; "complete" is default but more options are available (see ?dist)
+#' @param lower_bound the smallest number of profiles in the range of number of profiles to explore; defaults to 2
+#' @param upper_bound the largest number of profiles in the range of number of profiles to explore; defaults to 9
+#' @param r_squared_table if TRUE, then a table, rather than a plot, is returned; defaults to FALSE
+#' @return A list containing a ggplot2 object and a tibble for the R^2 values
+#' @examples
+#' df <- mtcars
+#' plot_r_squared(df, mpg, wt, hp, qsec, to_center = TRUE, lower_bound = 2, upper_bound = 4)
+#' @export
+#' #
 
+plot_r_squared <- function(df,    
+                           ...,
+                           to_center = FALSE,
+                           to_scale = FALSE,
+                           distance_metric = "squared_euclidean",
+                           linkage = "complete",
+                           lower_bound = 2, 
+                           upper_bound = 9,
+                           r_squared_table = FALSE) {
+    
+    out <- data.frame(
+        cluster = lower_bound:upper_bound,
+        r_squared_value = rep(NA, (upper_bound - lower_bound) + 1)
+    )
+    
+    message("################################")
+    
+    for (i in lower_bound:upper_bound) {
+        
+        message("Clustering data for iteration ", i)
+        
+        out[(i - 1), "r_squared_value"] <- 
+            suppressWarnings(
+                suppressMessages(
+                    create_profiles(df,
+                                    ...,
+                                    n_profiles = i, 
+                                    to_center = to_center, 
+                                    to_scale = to_scale, 
+                                    distance_metric = distance_metric, 
+                                    linkage = linkage)))[[5]]
+    }
+    
+    message("################################")
+    
+    out$r_squared_value <- round(out$r_squared_value, 3)
+    
+    out$cluster <- as.integer(out$cluster)
+    
+    p <- ggplot2::ggplot(out, ggplot2::aes_string(x = "cluster", y = "r_squared_value")) +
+        ggplot2::geom_point() +
+        ggplot2::geom_line()
+    
+    if (r_squared_table == T) {
+        suppressWarnings(return(out))
+    } else {
+        suppressWarnings(print(p))
+        suppressWarnings(return(p))
+    }
+}
+
+core_cross_validate <- function(df,
+                                ...,
+                                to_center,
+                                to_scale,
+                                n_profiles,
+                                distance_metric,
+                                linkage,
+                                k) {
+    
+    out <- dplyr::data_frame(k_iteration = rep(NA, k),
+                             kappa = rep(NA, k),
+                             percentage_agree = rep(NA, k))
+    
+    message("################################")
+    
+    for (i in seq(k)){
+        
+        message("Clustering data for iteration ", i)
+        
+        df <- dplyr::select(df, ...)
+        
+        df$ID <- 1:nrow(df)
+        
+        if (nrow(df) %% 2 == 0) {
+            dat1 <- dplyr::sample_n(df, nrow(df) / 2)
+            dat2 <- dplyr::filter(df, !(df$ID  %in% dat1$ID))
+        } else {
+            dat1 <- dplyr::sample_n(df, ceiling(nrow(df) / 2)) 
+            dat2 <- dplyr::filter(df, !(df$ID  %in% dat1$ID))
+            dat1 <- dat1[-nrow(dat1), ]
+        }
+        
+        dat1 <- dplyr::select(dat1, -ID)
+        row.names(dat1) <- NULL
+        dat2 <- dplyr::select(dat2, -ID)
+        
+        # step 1 (cluster half the data)
+        
+        two_prof_dat1 <- 
+            suppressWarnings(
+                suppressMessages(
+                    create_profiles(dat1, 
+                                    ...,
+                                    n_profiles = n_profiles, 
+                                    to_center = to_center, 
+                                    to_scale = to_center, 
+                                    distance_metric = distance_metric,
+                                    linkage = linkage)))
+        
+        # step 2 (cluster the other half of the data)
+        two_prof_dat2 <- 
+            suppressWarnings(
+                suppressMessages(
+                    create_profiles(dat2, 
+                                    ...,
+                                    n_profiles = n_profiles, 
+                                    to_center = to_center, 
+                                    to_scale = to_center, 
+                                    distance_metric = distance_metric,
+                                    linkage = linkage)))
+        
+        if (is.na(two_prof_dat1[[4]][1]) | is.na(two_prof_dat2[[4]][1]) ) {
+            message("")
+            message("Could not calculate agreement because k-means algorithm did not converge")
+            out$k_iteration[i] <- i
+            out$kappa[i] <- NA
+            out$percentage_agree[i] <- NA
+            next()
+        }
+        
+        # these are sample 1 data and cluster assignments
+        two_prof_cross_1 <- two_prof_dat1$clustered_raw_data 
+        
+        # these are sample 2 data and cluster assignments
+        two_prof_cross_2 <- two_prof_dat2$clustered_raw_data 
+        
+        # step 3 (Assign observations in one half (say, sample 2) . . . 
+        # . . . to the profile to which they are most similar in the other half (say, sample 1))
+        #reclassify by nearest neighbor
+        
+        two_prof_cross_2$cluster_nn <- class::knn1(dat1, dat2, two_prof_cross_1$cluster) 
+        
+        # step 4 (Re-code the profiles in sample 1 on the basis of which profile they are the most similar to in sample 2 . . . 
+        # . . . optimizing for the re-coding  that maximizes the agreement across all profiles)
+        
+        #two_prof_cross$cluster.f <- two_prof_cross$cluster
+        
+        two_prof_df <- dplyr::data_frame(orig_cluster = two_prof_cross_2$cluster, cluster_nn = two_prof_cross_2$cluster_nn)
+        two_prof_df$cluster_nn <- as.integer(two_prof_df$cluster_nn)
+        two_prof_tab <- table(two_prof_df)
+        
+        #solve assignment
+        res <- lpSolve::lp.assign(-two_prof_tab)
+        
+        l <- apply(res$solution > 0.5, 1, which)
+        
+        # step 5 (Calculate the agreement between re-coded sample 1 observations assigned to the sample 2 profile to which they are most similar . . . 
+        # . . . and the original sample 1 profiles
+        two_prof_cross_2$cluster_nn_rc <- l[two_prof_cross_2$cluster]
+        
+        recode_df <- dplyr::select(two_prof_cross_2, cluster_nn, cluster_nn_rc)
+        
+        try_kappa <- function(df) {
+            out <- tryCatch(
+                {
+                    x <- irr::kappam.fleiss(df)
+                    x$value
+                },
+                error = function(cond) {
+                    message(cond)
+                    return(NA)
+                },
+                warning = function(cond) {
+                    message(cond)
+                    return(NA)
+                }
+            )    
+            return(out)
+        }
+        
+        Kap <- try_kappa(recode_df)
+        agreement <- irr::agree(recode_df)
+        
+        out$k_iteration[i] <- i
+        out$kappa[i] <- round(as.numeric(Kap), 2)
+        out$percentage_agree[i] <- round(agreement$value / 100, 2)
+        
+    }
+    
+    message("################################")
+    
+    message(paste0("Mean Fleiss's Kappa for ", k, " iterations is ", round(mean(out$kappa, na.rm = T), 2)))
+    message(paste0("Mean percentage agreement for ", k, " iterations is ", round(mean(out$percentage_agree, na.rm = T), 2)))
+    
+    out
+    
+}
+
+#' Returns statistics from double-split cross validation
+#' @details Performs double-split cross validation and returns Cohen's Kappa and percentage agreement statistics.
+#' @param x A `prcr` object
+#' @param ... Additional arguments
+#' @return A ggplot2 object
+#' @export
+#' 
+
+cross_validate <- function(df,
+                           ...,
+                           to_center = FALSE,
+                           to_scale = FALSE,
+                           n_profiles,
+                           distance_metric = "squared_euclidean",
+                           linkage = "complete", 
+                           k = 30,
+                           lower_bound = 2,
+                           upper_bound = 9) {
+    
+    message("################################")
+    
+    if (n_profiles == "iterate") {
+        
+        out_list <- list()
+        
+        for (i in lower_bound:upper_bound){
+            
+            message(paste0("Clustering for n_profiles = ", i))
+            
+            out <- suppressWarnings(
+                suppressMessages(core_cross_validate(df,
+                                                     ...,
+                                                     to_center = to_center,
+                                                     to_scale = to_scale,
+                                                     n_profiles = i,
+                                                     distance_metric = distance_metric,
+                                                     linkage = linkage, 
+                                                     k = k)))
+            
+            out_list[[i - 1]] <- out
+            
+            
+            
+        }
+        
+        profile <- paste0("Profile ", lower_bound:upper_bound)
+        
+        mean_kappa <- purrr::map_dbl(out_list, function(x) mean(x$kappa, na.rm = T))
+        
+        mean_percentage_agree <- purrr::map_dbl(out_list, function(x) mean(x$percentage_agree, na.rm = T))
+        
+        out <- data.frame(profile, mean_kappa, mean_percentage_agree)
+        
+    } else {
+        
+        out <- core_cross_validate(df,
+                                   ...,
+                                   to_center = to_center,
+                                   to_scale = to_scale,
+                                   n_profiles = n_profiles,
+                                   distance_metric = distance_metric,
+                                   linkage = linkage, 
+                                   k = k)
+        
+    }
+    
+    return(out)
+    
+    message("################################")
+    
+}
+
+#' Concise summary of prcr cluster solution
+#' @details Prints a concise summary of prcr cluster solution
+#' @param object A `prcr` object
+#' @param ... Additional arguments
+#' @export
+
+summary.prcr <- function(object, ...){
+    cat(paste0(attributes(object)$n_profiles,
+               " cluster solution (R-squared = ", 
+               round(object$r_squared, 3), ")\n\n"))
+    cat("Profile n and means:\n\n")
+    print(object$clustered_processed_data)
+}
+
+#' Prints details of prcr cluster solution
+#' @details Prints details of of prcr cluster solution
+#' @param x A `prcr` object
+#' @param ... Additional arguments
+#' @export
+
+print.prcr <- function(x, ...){
+    cat("$clustered_processed_data\n\n")
+    print(x$clustered_processed_data)
+}
---
+++ R/helper.R
@@ -0,0 +1,200 @@
+# helper functions
+
+
+scale_vector <- function(x) {
+    x / stats::sd(x, na.rm = TRUE)
+}
+
+center_vector <- function(x) {
+    x - mean(x, na.rm = TRUE)
+}
+
+center_and_scale_vector <- function(x) {
+    if (stats::sd(x, na.rm = TRUE) == 0) {
+        x - mean(x, na.rm = TRUE)
+    } else {
+        (x - mean(x, na.rm = TRUE)) / stats::sd(x, na.rm = TRUE)
+    }
+}
+
+distance_function <- function(x, distance_metric){
+    if (distance_metric == "squared_euclidean") {
+        distance <- stats::dist(x, method = "euclidean")
+        distance <- distance ^ 2
+    } else {
+        distance <- stats::dist(x, method = distance_metric)
+    }
+    return(distance)
+}
+
+distance_function <- function(x, distance_metric){
+    if (distance_metric != "squared_euclidean") {
+        distance <- stats::dist(x, method = distance_metric)
+    } 
+    else {
+        distance <- stats::dist(x, method = "euclidean")
+        distance <- distance ^ 2
+    }
+    
+    distance
+}
+
+hclust_to_kmeans_function <- function(df, hclust_output, n_profiles){
+    h_clust_classification <- stats::cutree(hclust_output, n_profiles)
+    df %>% 
+        mutate(classification = h_clust_classification) %>% 
+        group_by(classification) %>% 
+        summarize_all(mean)
+}
+
+possibly_kmeans <- possibly(kmeans, NA)
+
+is.kmeans <- function(x) inherits(x, "kmeans")
+
+is.prcr <- function(x) inherits(x, "prcr")
+
+extract_hclust_output <- function(x){
+    attributes(x)$hclust_output
+}
+
+extract_kmeans_output <- function(x){
+    attributes(x)$kmeans_output
+}
+
+
+outlierHadi <- function(X) {
+    # -----------------------------------------------------------------
+    #  Hadi, Ali S. (1994), "A Modification of a Method for the
+    #  Detection of Outliers in Multivariate Samples," Journal of the
+    #  Royal Statistical Society (B), 2, 393-396.
+    # -----------------------------------------------------------------
+    n <- dim(X) [1]
+    p <- dim(X) [2]
+    h <- trunc((n + p + 1)/2)     
+    id <- 1:n
+    r <- p
+    out <- 0
+    cf <- (1 + ((p + 1)/(n - p)) + (2/(n - 1 - (3*p))) )^2
+    # cf <- (1 + ((p + 1)/(n - p)) + (1/(n - p - h)) )^2
+    alpha <- 0.05
+    tol <- max(10^-(p+5), 10^-12)
+    # -----------------------------------------------------------------
+    # **  Compute Mahalanobis distance
+    # -----------------------------------------------------------------
+    C <- apply(X, 2, mean)
+    S <- var(X)
+    if (det(S) < tol) stop ()
+    D <- mahalanobis(X, C, S)
+    mah.out <- 0
+    cv <- qchisq(1-(alpha/n), p)
+    for (i in 1:n) if (D[i] >= cv) mah.out <- cbind(mah.out, i)
+    mah.out <- mah.out[-1]
+    mah <- sqrt(D)
+    Xbar <- C
+    Covariance <- S   #
+    # ----------------------------------------------------------------
+    # **  Step 0
+    # ----------------------------------------------------------------
+    #  **  Compute Di(Cm, Sm)
+    C <- apply(X, 2, median)
+    #original code was 
+    #  C <- t(array(C, dim = c(n, p)))
+    #but resulted in nonconformable arrays. 
+    #so i removed the transpose
+    C <- array(C, dim = c(n, p))
+    Y <- X - C
+    S <- ((n - 1)^-1)*(t(Y) %*% Y)
+    D <- mahalanobis(X, C[1, ], S)
+    Z <- sort.list(D)
+    # ----------------------------------------------------------------
+    #  **  Compute Di(Cv, Sv)
+    repeat {
+        Y <- X[Z[1:h], ]
+        C <- apply(Y, 2, mean)
+        S <- var(Y)
+        if (det(S) > tol) {
+            D <- mahalanobis(X, C, S)
+            Z <- sort.list(D); break }
+        else h <- h + 1
+    }
+    # ----------------------------------------------------------------
+    #  **  Step 1
+    # ----------------------------------------------------------------
+    repeat {
+        r <- r + 1
+        if ( h < r) break
+        Y <- X[Z[1:r],]
+        C <- apply(Y, 2, mean)
+        S <- var(Y)
+        if (det(S) > tol) {
+            D <- mahalanobis(X, C, S)
+            Z <- sort.list(D) }
+    }
+    #**  Step 3
+    # ----------------------------------------------------------------
+    #  **  Compute Di(Cb, Sb)
+    repeat {
+        Y <- X[Z[1:h],]
+        C <- apply(Y, 2, mean)
+        S <- var(Y)
+        if (det(S) > tol) {
+            D <- mahalanobis(X, C, S)
+            Z <- sort.list(D)
+            if (D[Z[h + 1]] >= (cf*qchisq(1-(alpha/n), p))) {
+                out <- Z[(h + 1) : n]
+                break }
+            else { h <- h + 1
+            if (n <= h) break }
+        }
+        else { h <- h + 1
+        if (n <= h) break }
+    }
+    D <- sqrt(D/cf)
+    dst <- cbind(id, mah, D)
+    Outliers <- out
+    Cb <- C;
+    Sb <- S
+    Distances <- dst
+    result <- list(Xbar = Xbar, Covariance = Covariance, mah.out = mah.out, 
+                   Outliers = Outliers, Cb = Cb, Sb = Sb, Distances = Distances )
+    class( result ) <- "outlierHadi"
+    return( result )
+}
+
+uv_outlier_detector <- function(x, na.rm = T, ...) {
+    # need to figure out where this came from - from a SO question, can probably re-write
+    qnt <- quantile(x, probs=c(.25, .75), na.rm = na.rm, ...)
+    H <- 1.5 * IQR(x, na.rm = na.rm)
+    y <- x
+    y[x < (qnt[1] - H)] <- NA
+    y[x > (qnt[2] + H)] <- NA
+    y
+}
+
+remove_uv_out_func <- function(data){
+    x <- sapply(data, uv_outlier_detector)
+    return(x)
+}
+
+remove_uv_main_func <- function(data, removed_obs_df, cases_to_keep){
+    data_tmp <- remove_uv_out_func(data) # makes uv outliers na
+    print(paste0("### Note: ", sum(is.na(data_tmp)), " cases with univariate outliers out of ", nrow(data_tmp), " cases removed, so ", nrow(data_tmp) - sum(is.na(data_tmp)), " used in subsequent analysis ###"))
+    if(any(is.na(data_tmp))){
+        x <- removed_obs_df[cases_to_keep, ]
+        y <- !complete.cases(data_tmp)
+        z <- x$row[y]
+        removed_obs_df$reason_removed[z] <- "uniivariate_outlier"
+    }
+    data_out <- data_tmp[complete.cases(data_tmp), ]
+    if(any(is.na(data_tmp))){
+        data_out <- list(data_out, removed_obs_df, uv_outliers_boolean_vector = y)
+    } else{
+        data_out <- list(data_out, removed_obs_df, uv_outliers_boolean_vector = NULL)
+    }
+    return(data_out)
+}
+
+detect_outliers <- function(df) {
+    outlierHadi(as.matrix(df))
+    # need to add UV outlier detector
+}
